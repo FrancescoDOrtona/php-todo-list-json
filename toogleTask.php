661b3c4d4e8386c762c5343452682aec8dfdf0a1
FrancescoDOrtona <francescod.97@hotmail.it>
2023-11-27 15:58:13 +0100

    Fix: toogle task fix in php code

--- toogleTask.php
+++ toogleTask.php
@@ -1,5 +1,5 @@
 <?php
-$index = $_POST["index"];
+$index = intval($_POST["index"]);
 $todos_json = file_get_contents('./todos.json');
 $todos = json_decode($todos_json, true);
 
@@ -7,13 +7,14 @@ $response = [
     'succes' => true,
 ];
 
-// $todos[$index['done']] = $todos[$index[!('done')]];
-$todos[$index['done']] = !('done');
-// $response['todos'] = $todos[$index[!'done']];
+if ($index >= 0) {
+$todos[$index]['done'] = !$todos[$index]['done'];
+
 
 $response['todos'] = $todos;
 $todos_string = json_encode($todos);
 file_put_contents('./todos.json', $todos_string);
+}
 
 header('Content-type: application/json');
 echo json_encode($response);
